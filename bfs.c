aad6820c494ee45ff5c422bb7cc0d1a86f12e5fe
kgouin <kayagouin@cmail.carleton.ca>
2022-06-16 16:08:23 -0400

    improved BFS algorithm

--- bfs.c
+++ bfs.c
@@ -40,23 +40,33 @@ void BFS_init(struct bfs_struct* s){
 
 int* BFS(struct bfs_struct* s){
 	s->bfs = malloc((s->v)*sizeof(int));
-	//for (int i = 0; i < s->v; i++){ //for testing purposes
-	//	bfs_array[i] = -1;
-	//}
+	int* q = malloc((s->v)*sizeof(int));
 	int* seen = calloc((s->v),sizeof(int));
 
-	int c = 0;
 	s->bfs[0] = 0;
+	q[0] = 0;
 	seen[0] = 1;
-	for (int i = 0; i < s->v; i++){
-		for (int j = 0; j < s->n[i]; j++){
-			if (!seen[s->a[i][j]]){
-				s->bfs[++c] = s->a[i][j];
-				seen[s->a[i][j]] = 1;
+
+	int start = 0;
+	int end = 1;
+	//we use start and end markers to implement a queue, or a circular array
+	int temp;
+	while (start < end){
+		//'remove' elt at start of q (aka. increase start marker)
+		temp = q[start];
+		start++;
+		//add all neighbours of 'removed' elt to q and increase end marker
+		for (int k = 0; k < s->n[temp]; k++){
+			if (!seen[s->a[temp][k]]){
+				s->bfs[end] = s->a[temp][k];
+				q[end] = s->a[temp][k];
+				seen[s->a[temp][k]] = 1;
+				end++;
 			}
 		}
 	}
 
+	free(q);
 	free(seen);
 
 	return s->bfs;
