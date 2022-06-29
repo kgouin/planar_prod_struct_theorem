40cc22b8fa7fdbfbffcd919c9263dcd877f2f92e
kgouin <kayagouin@cmail.carleton.ca>
2022-06-29 10:57:53 -0400

    correct tripod labeling

--- tripod.c
+++ tripod.c
@@ -11,15 +11,15 @@ void tripod_init(struct bfs_struct* b, struct rmq_struct* r){
 	LCA_init(r, b->ct, (2*(b->f))-1);
 	printf("initialization complete\n");
 
-	int* acc = malloc((((r->n)/2)+1)*sizeof(int)); //memory leak
+	int* acc = malloc((b->v)*sizeof(int)); //memory leak
 
 	for (int k = 0; k < (((r->n)/2)+1); k++){
 		acc[k] = -1; //vertices labeled with -1 do not yet belong to a tripod
 	}
 	//label the three edges incident to face 0 as belonging to three exterior tripods
-	acc[b->sim[0][0]] = -2;
-	acc[b->sim[0][1]] = -3;
-	acc[b->sim[0][2]] = -4;
+	acc[b->sim[0][0]] = (b->v);
+	acc[b->sim[0][1]] = (b->v)+1;
+	acc[b->sim[0][2]] = (b->v)+2;
 
 	//start decomposition with the three triangles adjacent to outer face
 	tripod(b, r, b->tri[0][0], b->tri[0][1], b->tri[0][2], acc);
@@ -44,23 +44,24 @@ int* tripod(struct bfs_struct* b, struct rmq_struct* r, int f1, int f2, int f3, 
 
 		//store tripod (aka. for each vertex incident to sp, walk up bfs tree until you hit an edge belonging to another tripod)
 		//b->sim[sp][0], b->sim[sp][1], b->sim[sp][2] are the vertices incident to our sperner triangle
-		u = acc[b->sim[sp][0]];
-		while (u == -1){
-			acc[b->sim[sp][0]] = sp;
-			u = acc[b->bt[b->sim[sp][0]]];
+		u = b->sim[sp][0];
+		while (acc[u] == -1){
+			acc[u] = sp;
+			u = b->bt[b->sim[sp][0]];
 		}
-		u = acc[b->sim[sp][1]];
-		while (u == -1){
-			acc[b->sim[sp][1]] = sp;
-			u = acc[b->bt[b->sim[sp][1]]];
+		u = b->sim[sp][1];
+		while (acc[u] == -1){
+			acc[u] = sp;
+			u = b->bt[b->sim[sp][1]];
 		}
-		u = acc[b->sim[sp][2]];
-		while (u == -1){
-			acc[b->sim[sp][2]] = sp;
-			u = acc[b->bt[b->sim[sp][2]]];
+		u = b->sim[sp][2];
+		while (acc[u] == -1){
+			acc[u] = sp;
+			u = b->bt[b->sim[sp][2]];
 		}
 
 		//recurse (we'll need to handle the case where an input face is right on a boundary)
+		//look at tri[sp][0], tri[sp][1], tri[sp][2] to find triangles adjacent to our newly found sperner triangle
 	}
 
 	else { //two of the three faces are the same, the other is different
@@ -69,20 +70,20 @@ int* tripod(struct bfs_struct* b, struct rmq_struct* r, int f1, int f2, int f3, 
 
 		//store tripod (aka. for each vertex incident to sp, walk up bfs tree until you hit an edge belonging to another tripod)
 		//b->sim[sp][0], b->sim[sp][1], b->sim[sp][2] are the vertices incident to our sperner triangle
-		u = acc[b->sim[sp][0]];
-		while (u == -1){
-			acc[b->sim[sp][0]] = sp;
-			u = acc[b->bt[b->sim[sp][0]]];
+		u = b->sim[sp][0];
+		while (acc[u] == -1){
+			acc[u] = sp;
+			u = b->bt[b->sim[sp][0]];
 		}
-		u = acc[b->sim[sp][1]];
-		while (u == -1){
-			acc[b->sim[sp][1]] = sp;
-			u = acc[b->bt[b->sim[sp][1]]];
+		u = b->sim[sp][1];
+		while (acc[u] == -1){
+			acc[u] = sp;
+			u = b->bt[b->sim[sp][1]];
 		}
-		u = acc[b->sim[sp][2]];
-		while (u == -1){
-			acc[b->sim[sp][2]] = sp;
-			u = acc[b->bt[b->sim[sp][2]]];
+		u = b->sim[sp][2];
+		while (acc[u] == -1){
+			acc[u] = sp;
+			u = b->bt[b->sim[sp][2]];
 		}
 
 		//recurse (we'll need to handle the case where an input face is right on a boundary)
@@ -91,7 +92,7 @@ int* tripod(struct bfs_struct* b, struct rmq_struct* r, int f1, int f2, int f3, 
 	printf("%d is one of our sperner triangles\n", sp); //sperner triangle is correctly identified
 	
 	printf("acc = ");
-	for (int k = 0; k < ((r->n)/2)+1; k++){
+	for (int k = 0; k < (b->v); k++){
 		printf("%d ", acc[k]);
 	}
 	printf("\n");
