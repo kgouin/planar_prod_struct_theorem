d6440535a0820d633b0c3f4a733b264275447e61
kgouin <kayagouin@cmail.carleton.ca>
2022-08-09 09:48:59 -0400

    more progress on subproblem alignment

--- tripod.c
+++ tripod.c
@@ -9,6 +9,8 @@ void tripod_init(struct bfs_struct* b, struct rmq_struct* r, struct tripod_decom
 	BFS_init(b);
 	BFS(b);
 	LCA_init(r, b->ct, (2*(b->f))-1);
+	printf("--------------------------------------------------------------------------------------------------------------------------------\n");
+	printf("--------------------------------------------------------------------------------------------------------------------------------\n");
 	printf("initialization complete\n");
 
 	printf("bfs tree = "); //testing
@@ -124,15 +126,17 @@ void orient_subproblems(struct bfs_struct* b, struct tripod_decomposition_struct
 		partial_match = 0;
 		double_match = 0;
 		for (int i = 0; i < 3; i++){ //look at all three vertices of our v_a_op triangle
-			if ((acc[b->sim[t->v_a_op][i]] == acc[t->v_a] || acc[b->sim[t->v_a_op][i]] == acc[t->v_b]) && !partial_match){
-				partial_match = 1;
-			}
-			else if ((acc[b->sim[t->v_a_op][i]] == acc[t->v_a] || acc[b->sim[t->v_a_op][i]] == acc[t->v_b]) && partial_match){
-				double_match = 1;
+			if (acc[b->sim[t->v_a_op][i]] == acc[t->v_a]){
+				for (int j = 0; j < 3; j++){
+					if (acc[b->sim[t->v_a_op][j]] == acc[t->v_b]){
+						double_match = 1;
+					}
+				}
 			}
 		}
 		if (double_match){
 			printf("NO SWITCH\n");
+			printf("01\n");
 			//v_a_op is part of the subproblem between vertex v_a and vertex v_b
 			//v_b_op is part of the subproblem between vertex v_b and vertex v_c
 			//v_c_op is part of the subproblem between vertex v_c and vertex v_a
@@ -142,11 +146,12 @@ void orient_subproblems(struct bfs_struct* b, struct tripod_decomposition_struct
 			partial_match = 0;
 			double_match = 0;
 			for (int i = 0; i < 3; i++){ //look at all three vertices of our v_b_op triangle
-				if ((acc[b->sim[t->v_b_op][i]] == acc[t->v_a] || acc[b->sim[t->v_b_op][i]] == acc[t->v_b]) && !partial_match){
-					partial_match = 1;
-				}
-				else if ((acc[b->sim[t->v_b_op][i]] == acc[t->v_a] || acc[b->sim[t->v_b_op][i]] == acc[t->v_b]) && partial_match){
-					double_match = 1;
+				if (acc[b->sim[t->v_b_op][i]] == acc[t->v_a]){
+					for (int j = 0; j < 3; j++){
+						if (acc[b->sim[t->v_b_op][j]] == acc[t->v_b]){
+							double_match = 1;
+						}
+					}
 				}
 			}
 			if (double_match){
@@ -160,6 +165,7 @@ void orient_subproblems(struct bfs_struct* b, struct tripod_decomposition_struct
 			}
 			else { //no double match. v_c_op must match.
 				printf("SWITCH 2\n");
+				printf("01\n");
 				tprint(t);
 
 				int v_temp_op = t->v_c_op;
@@ -177,15 +183,17 @@ void orient_subproblems(struct bfs_struct* b, struct tripod_decomposition_struct
 		partial_match = 0;
 		double_match = 0;
 		for (int i = 0; i < 3; i++){ //look at all three vertices of our v_a_op triangle
-			if ((acc[b->sim[t->v_a_op][i]] == acc[t->v_a] || acc[b->sim[t->v_a_op][i]] == acc[t->v_b_next]) && !partial_match){
-				partial_match = 1;
-			}
-			else if ((acc[b->sim[t->v_a_op][i]] == acc[t->v_a] || acc[b->sim[t->v_a_op][i]] == acc[t->v_b_next]) && partial_match){
-				double_match = 1;
+			if (acc[b->sim[t->v_a_op][i]] == acc[t->v_a]){
+				for (int j = 0; j < 3; j++){
+					if (acc[b->sim[t->v_a_op][j]] == acc[t->v_b_next]){
+						double_match = 1;
+					}
+				}
 			}
 		}
 		if (double_match){
 			printf("NO SWITCH\n");
+			printf("02\n");
 			//v_a_op is part of the subproblem between vertex v_a and vertex v_b
 			//v_b_op is part of the subproblem between vertex v_b and vertex v_c
 			//v_c_op is part of the subproblem between vertex v_c and vertex v_a
@@ -195,11 +203,12 @@ void orient_subproblems(struct bfs_struct* b, struct tripod_decomposition_struct
 			partial_match = 0;
 			double_match = 0;
 			for (int i = 0; i < 3; i++){ //look at all three vertices of our v_b_op triangle
-				if ((acc[b->sim[t->v_b_op][i]] == acc[t->v_a] || acc[b->sim[t->v_b_op][i]] == acc[t->v_b_next]) && !partial_match){
-					partial_match = 1;
-				}
-				else if ((acc[b->sim[t->v_b_op][i]] == acc[t->v_a] || acc[b->sim[t->v_b_op][i]] == acc[t->v_b_next]) && partial_match){
-					double_match = 1;
+				if (acc[b->sim[t->v_b_op][i]] == acc[t->v_a]){
+					for (int j = 0; j < 3; j++){
+						if (acc[b->sim[t->v_b_op][j]] == acc[t->v_b_next]){
+							double_match = 1;
+						}
+					}
 				}
 			}
 			if (double_match){
@@ -213,6 +222,7 @@ void orient_subproblems(struct bfs_struct* b, struct tripod_decomposition_struct
 			}
 			else { //no double match. v_c_op must match.
 				printf("SWITCH 2\n");
+				printf("02\n");
 				tprint(t);
 
 				int v_temp_op = t->v_c_op;
@@ -230,15 +240,17 @@ void orient_subproblems(struct bfs_struct* b, struct tripod_decomposition_struct
 		partial_match = 0;
 		double_match = 0;
 		for (int i = 0; i < 3; i++){ //look at all three vertices of our v_a_op triangle
-			if ((acc[b->sim[t->v_a_op][i]] == acc[t->v_a_next] || acc[b->sim[t->v_a_op][i]] == acc[t->v_b]) && !partial_match){
-				partial_match = 1;
-			}
-			else if ((acc[b->sim[t->v_a_op][i]] == acc[t->v_a_next] || acc[b->sim[t->v_a_op][i]] == acc[t->v_b]) && partial_match){
-				double_match = 1;
+			if (acc[b->sim[t->v_a_op][i]] == acc[t->v_a_next]){
+				for (int j = 0; j < 3; j++){
+					if (acc[b->sim[t->v_a_op][j]] == acc[t->v_b]){
+						double_match = 1;
+					}
+				}
 			}
 		}
 		if (double_match){
 			printf("NO SWITCH\n");
+			printf("03\n");
 			//v_a_op is part of the subproblem between vertex v_a and vertex v_b
 			//v_b_op is part of the subproblem between vertex v_b and vertex v_c
 			//v_c_op is part of the subproblem between vertex v_c and vertex v_a
@@ -248,11 +260,12 @@ void orient_subproblems(struct bfs_struct* b, struct tripod_decomposition_struct
 			partial_match = 0;
 			double_match = 0;
 			for (int i = 0; i < 3; i++){ //look at all three vertices of our v_b_op triangle
-				if ((acc[b->sim[t->v_b_op][i]] == acc[t->v_a_next] || acc[b->sim[t->v_b_op][i]] == acc[t->v_b]) && !partial_match){
-					partial_match = 1;
-				}
-				else if ((acc[b->sim[t->v_b_op][i]] == acc[t->v_a_next] || acc[b->sim[t->v_b_op][i]] == acc[t->v_b]) && partial_match){
-					double_match = 1;
+				if (acc[b->sim[t->v_b_op][i]] == acc[t->v_a_next]){
+					for (int j = 0; j < 3; j++){
+						if (acc[b->sim[t->v_b_op][j]] == acc[t->v_b]){
+							double_match = 1;
+						}
+					}
 				}
 			}
 			if (double_match){
@@ -266,6 +279,7 @@ void orient_subproblems(struct bfs_struct* b, struct tripod_decomposition_struct
 			}
 			else { //no double match. v_c_op must match.
 				printf("SWITCH 2\n");
+				printf("03\n");
 				tprint(t);
 
 				int v_temp_op = t->v_c_op;
@@ -283,15 +297,17 @@ void orient_subproblems(struct bfs_struct* b, struct tripod_decomposition_struct
 		partial_match = 0;
 		double_match = 0;
 		for (int i = 0; i < 3; i++){ //look at all three vertices of our v_a_op triangle
-			if ((acc[b->sim[t->v_a_op][i]] == acc[t->v_a_next] || acc[b->sim[t->v_a_op][i]] == acc[t->v_b_next]) && !partial_match){
-				partial_match = 1;
-			}
-			else if ((acc[b->sim[t->v_a_op][i]] == acc[t->v_a_next] || acc[b->sim[t->v_a_op][i]] == acc[t->v_b_next]) && partial_match){
-				double_match = 1;
+			if (acc[b->sim[t->v_a_op][i]] == acc[t->v_a_next]){
+				for (int j = 0; j < 3; j++){
+					if (acc[b->sim[t->v_a_op][j]] == acc[t->v_b_next]){
+						double_match = 1;
+					}
+				}
 			}
 		}
 		if (double_match){
 			printf("NO SWITCH\n");
+			printf("04\n");
 			//v_a_op is part of the subproblem between vertex v_a and vertex v_b
 			//v_b_op is part of the subproblem between vertex v_b and vertex v_c
 			//v_c_op is part of the subproblem between vertex v_c and vertex v_a
@@ -301,11 +317,12 @@ void orient_subproblems(struct bfs_struct* b, struct tripod_decomposition_struct
 			partial_match = 0;
 			double_match = 0;
 			for (int i = 0; i < 3; i++){ //look at all three vertices of our v_b_op triangle
-				if ((acc[b->sim[t->v_b_op][i]] == acc[t->v_a_next] || acc[b->sim[t->v_b_op][i]] == acc[t->v_b_next]) && !partial_match){
-					partial_match = 1;
-				}
-				else if ((acc[b->sim[t->v_b_op][i]] == acc[t->v_a_next] || acc[b->sim[t->v_b_op][i]] == acc[t->v_b_next]) && partial_match){
-					double_match = 1;
+				if (acc[b->sim[t->v_b_op][i]] == acc[t->v_a_next]){
+					for (int j = 0; j < 3; j++){
+						if (acc[b->sim[t->v_b_op][j]] == acc[t->v_b_next]){
+							double_match = 1;
+						}
+					}
 				}
 			}
 			if (double_match){
@@ -319,6 +336,7 @@ void orient_subproblems(struct bfs_struct* b, struct tripod_decomposition_struct
 			}
 			else { //no double match. v_c_op must match.
 				printf("SWITCH 2\n");
+				printf("04\n");
 				tprint(t);
 
 				int v_temp_op = t->v_c_op;
@@ -335,6 +353,8 @@ void decompose(struct bfs_struct* b, struct rmq_struct* r, struct tripod_decompo
 	int b_incomplete = 1;
 	int c_incomplete = 1;
 	int stop = 0;
+
+	//if (f1 == 34 && f2 == 34 && f3 == 35) exit(0);
 
 	int v_a = t->v_a;
 	int v_a_next = t->v_a_next;
@@ -400,10 +420,62 @@ void decompose(struct bfs_struct* b, struct rmq_struct* r, struct tripod_decompo
 			tripod(b, r, t, v_a_op, v_b_r, v_a_mirror, acc, acc2);
 		}
 	}
+	else if (acc[v_a] != sp && acc[v_b] != sp){ //leg a is empty && leg b is empty
+		printf("special subproblem a\n");
+		//int next;
+		//int mirr;
+		//(v_a_op == sp) ? ((v_a_op == v_b_op) ? (next = v_c_op) : (next = v_b_op)) : (next = v_a_op);
+		/*if (acc2[v_a_op] != -1){ //if v_a_op is a sperner triangle
+			printf("v_a_op == some sp\n");
+			if (v_a_op == v_b_op){
+				printf("v_a_op == v_b_op\n");
+				next = v_c_op;
+				mirr = v_c_mirror;
+			}
+			else {
+				printf("v_a_op != v_b_op\n");
+				next = v_b_op;
+				mirr = v_b_mirror;
+			}
+		}
+		else{
+			printf("v_a_op != some sp\n");
+			next = v_a_op;
+			mirr = v_a_mirror;
+		}*/
+		/*if (acc2[v_a_op] != -1 || acc2[v_a_mirror] != -1){ //don't use a
+			if (acc2[v_b_op] != -1 || acc2[v_b_mirror] != -1){ //don't use b
+				if (acc2[v_c_op] != -1 || acc2[v_c_mirror] != -1){ //don't use c
+					next = -1;
+					mirr = -1;
+				}
+				else { //use c
+					next = v_c_op;
+					mirr = v_c_mirror;
+				}
+			}
+			else { //use b
+				next = v_b_op;
+				mirr = v_b_mirror;
+			}
+		}
+		else { //use a
+			next = v_a_op;
+			mirr = v_a_mirror;
+		}
+
+		printf("next = %d, mirr = %d\n", next, mirr);
+		printf("special subproblem a for sp %d will be on faces %d, %d, %d\n", sp, next, mirr, mirr);
+		tripod(b, r, t, next, mirr, mirr, acc, acc2);*/
+		/*if (acc2[v_a_op] == -1){ //if op is not a sperner triangle (aka if we're not on a border)
+			if (acc2[v_a_mirror] == -1 && acc2[v_a_op] == -1){ //make sure we're not recursing on a previous sp
+				printf("special subproblem 1 for sp %d will be on faces %d, %d, %d\n", sp, v_a_op, v_a_mirror, v_a_mirror);
+				tripod(b, r, t, v_a_op, v_a_mirror, v_a_mirror, acc, acc2);
+			}
+		}*/
+	}
 
 	if (!stop){ //if there are more than one subproblems, carry on
-		printf("working on subproblem %d\n", sp);
-		printf("v_b = %d, v_c = %d, acc[v_b] = %d, acc[v_c] = %d\n", v_b, v_c, acc[v_b], acc[v_c]);
 		if (acc[v_b] == sp && acc[v_c] == sp){ //leg b is non-empty && leg c is non-empty
 			b_incomplete = 0;
 			v_b_l = b->il[v_b][b->pin[v_b]];
@@ -443,6 +515,18 @@ void decompose(struct bfs_struct* b, struct rmq_struct* r, struct tripod_decompo
 				printf("subproblem b for sp %d will be on faces %d, %d, %d\n", sp, v_b_op, v_c_r, v_b_mirror);
 				tripod(b, r, t, v_b_op, v_c_r, v_b_mirror, acc, acc2);
 			}
+		}
+		else if (acc[v_b] != sp && acc[v_c] != sp){ //leg b is empty && leg c is empty
+			//printf("special case b\n");
+			//int next;
+			//(v_b_op == sp) ? ((v_b_op == v_c_op) ? (next = v_a_op) : (next = v_c_op)) : (next = v_b_op);
+			//printf("next = %d, v_b_mirror = %d\n", next, v_b_mirror);
+			/*if (acc2[v_b_op] == -1){ //if op is not a sperner triangle (aka if we're not on a border)
+				if (acc2[v_b_mirror] == -1 && acc2[v_b_op] == -1){ //make sure we're not recursing on a previous sp
+					printf("special subproblem 2 for sp %d will be on faces %d, %d, %d\n", sp, v_b_op, v_b_mirror, v_b_mirror);
+					tripod(b, r, t, v_b_op, v_b_mirror, v_b_mirror, acc, acc2);
+				}
+			}*/
 		}
 
 		if (acc[v_c] == sp && acc[v_a] == sp){ //leg c is non-empty && leg a is non-empty
@@ -485,15 +569,45 @@ void decompose(struct bfs_struct* b, struct rmq_struct* r, struct tripod_decompo
 				tripod(b, r, t, v_c_op, v_a_r, v_c_mirror, acc, acc2);
 			}
 		}
+		else if (acc[v_c] != sp && acc[v_a] != sp){ //leg c is empty && leg a is empty
+			//printf("special case c\n");
+			//int next;
+			//(v_c_op == sp) ? ((v_c_op == v_a_op) ? (next = v_b_op) : (next = v_a_op)) : (next = v_c_op);
+			//printf("next = %d, v_c_mirror = %d\n", next, v_c_mirror);
+			/*if (acc2[v_c_mirror] == -1 && acc2[v_c_op] == -1){ //make sure we're not recursing on a previous sp
+				if (acc2[v_c_op] == -1){ //if op is not a sperner triangle (aka if we're not on a border)
+					printf("special subproblem 3 for sp %d will be on faces %d, %d, %d\n", sp, v_c_op, v_c_mirror, v_c_mirror);
+					tripod(b, r, t, v_c_op, v_c_mirror, v_c_mirror, acc, acc2);
+				}
+			}*/
+		}
 
-		if (a_incomplete && b_incomplete && c_incomplete){
+		/*if (a_incomplete && b_incomplete && c_incomplete){
+			printf("in special if\n");
 			int next;
+
 			(v_a_op == v_b_op) ? (next = v_c_op) : (next = v_b_op);
+			printf("acc2[v_a_mirror] = acc2[%d] = %d\n", v_a_mirror, acc2[v_a_mirror]);
+			printf("acc2[v_a_op] = acc2[%d] = %d\n", v_a_op, acc2[v_a_op]);
 			if (acc2[v_a_mirror] == -1 && acc2[next] == -1){ //make sure we're not recursing on a previous sp
-				printf("subproblem a for sp %d will be on faces %d, %d, %d\n", sp, next, v_a_mirror, v_a_mirror);
+				printf("v_a_op = %d, v_b_op = %d, v_c_op = %d\n", v_a_op, v_b_op, v_c_op);
+				printf("special subproblem for sp %d will be on faces %d, %d, %d\n", sp, next, v_a_mirror, v_a_mirror);
 				tripod(b, r, t, next, v_a_mirror, v_a_mirror, acc, acc2);
 			}
-		}
+
+			if (acc2[v_a_mirror] == -1 && acc2[v_a_op] == -1){ //make sure we're not recursing on a previous sp
+				printf("special subproblem 1 for sp %d will be on faces %d, %d, %d\n", sp, v_a_op, v_a_mirror, v_a_mirror);
+				tripod(b, r, t, v_a_op, v_a_mirror, v_a_mirror, acc, acc2);
+			}
+			if (acc2[v_b_mirror] == -1 && acc2[v_b_op] == -1){ //make sure we're not recursing on a previous sp
+				printf("special subproblem 2 for sp %d will be on faces %d, %d, %d\n", sp, v_b_op, v_b_mirror, v_b_mirror);
+				tripod(b, r, t, v_b_op, v_b_mirror, v_b_mirror, acc, acc2);
+			}
+			if (acc2[v_c_mirror] == -1 && acc2[v_c_op] == -1){ //make sure we're not recursing on a previous sp
+				printf("special subproblem 3 for sp %d will be on faces %d, %d, %d\n", sp, v_c_op, v_c_mirror, v_c_mirror);
+				tripod(b, r, t, v_c_op, v_c_mirror, v_c_mirror, acc, acc2);
+			}
+		}*/
 	}
 }
 
